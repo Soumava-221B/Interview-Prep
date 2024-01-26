9200e2fdf7a890b9299d512d4207511ecbdbfcdb
Soumava Das <soumavadas06@gmail.com>
2024-01-26 00:24:51 +0000

    middle of the linked list

---
+++ middle_linkedList.java
@@ -0,0 +1,29 @@
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode m = head;
+        ListNode e = head;
+
+        while (e != null && e.next != null) {
+            m = m.next;
+            e = e.next.next;
+        }
+
+        return m;
+    }
+}
